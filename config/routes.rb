a26054e7368f48b7fb3702b5a2f0ea97a2cb09fa
Billy Kimble <basslines@gmail.com>
2014-07-14 14:57:13 -0700

    oauth controller

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,9 @@ Rails.application.routes.draw do
   namespace :admin do
     resources :stories
   end
+  
+  post 'oauth/auth' => 'oauth#auth'
+  get 'oauth/auth' => 'oauth#auth'
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
